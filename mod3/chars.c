f8772f227390476b57eb365d2f2aa8e8ea312f4c
Trevor <trevorculp@mail.weber.edu>
2018-01-23 10:01:59 -0700

    Fixed ENTER char issue

--- mod3/chars.c
+++ mod3/chars.c
@@ -24,19 +24,23 @@
 // Main Function
 int main()
 {
-    char input, dummy;
+    char input;
     int age;
     printf("Do you like to code?:\n");
     scanf("%c", &input);
-    printf("enter your age\n");
-    scanf("%d", &age);
-    printf("Your age is: [%d]\n", age);
+
 
     printf("(1.)You entered: [%c]\n", input);
     printf("Do you really like to code?:\n");
-    scanf("%c %c", &dummy, &input);
+    /*use an empty space before %c, 
+     *this will take care of the previous enter char */
+    scanf(" %c", &input);
     printf("(2.)You entered: [%c]\n", input);
     printf("(3.)You entered: [%c]\n", input);
+
+    printf("enter your age\n");
+    scanf("%d", &age);
+    printf("Your age is: [%d]\n", age);
     return 0;
 }
 // Function Definitions
