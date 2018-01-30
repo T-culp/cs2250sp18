44e0021dcb4914d2ddb03f527fbf685058a5473b
Trevor <trevorculp@mail.weber.edu>
2018-01-30 10:14:36 -0700

    initial commit

---
+++ mod4/multibranch.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description: Nested ranches 
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:24 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Trevor Culp (), Trevorculp@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if (income)  // if income != 0 
+    {
+        printf("Income is: $%d.\n", income);
+    }
+    else
+    {
+        printf("No income.\n");
+    }
+    return 0;
+}
+// Function Definitions
+
+
