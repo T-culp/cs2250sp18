c0f922af11c2b597bd054cb40bdc3c7ec0c7ab2b
Trevor <trevorculp@mail.weber.edu>
2018-02-06 12:40:45 -0700

    update files

--- mod4/hw3ch3pr1.c
+++ mod4/hw3ch3pr1.c
@@ -30,37 +30,73 @@ int main()
     int oil = 35;
     int tire = 19;
     int wash = 7;
-    char service [15];
+    char service1 [15];
+    char service2 [15];
+
 
 
     //Display options to user
     printf("Davy's auto shop services\n Oil change -- $35\n Tire rotation -- $19\n Car wash -- $7\n");
     //Prompt user for desired service
     printf("Enter desired auto service:\n");
-    scanf("%[^\n] ", service);
+    scanf("%s ", service1);
 
 
     //Determine what service user selected, display relevant info.
-    if (strcmp(service, "Oil change") == 0) 
-        {
-            printf("You entered: %s.\n", service);
-            printf("Cost of oil change: $%d\n", oil);
-        }
-    else if (strcmp(service, "Tire rotation") == 0) 
-        {  
-            printf("You entered: %s\n", service);
-            printf("Cost of tire rotation: $%d\n", tire);
+    if (strcmp(service1, "Oil") == 0) 
+    {
+        scanf("%s", service2);
+        if (strcmp(service2, "change") == 0)
+                {
+                printf("You entered: %s %s.\n", service1, service2);
+                printf("Cost of oil change: $%d\n", oil);
+                }
+                else
+                {
+                printf("You entered: %s\n", service1);
+                printf("Requested service is not recognized\n");
+                }//End nested if/else
 
-        }
-    else if (strcmp(service, "Car wash") == 0) 
-        {  
-            printf("You entered: %s\n", service);
-            printf("Cost of car wash: $%d\n", wash);
+     }//End if
 
-        }
+    else if (strcmp(service1, "Tire") == 0) 
+    {  
+           
+            scanf("%s", service2);
+            if (strcmp(service2, "rotation") == 0)
+                    {
+                    printf("You entered: %s %s.\n", service1, service2);
+                    printf("Cost of tire rotation: $%d\n", tire);
+                    }
+            else
+                    {
+                    printf("You entered: %s\n", service1);
+                    printf("Requested service is not recognized\n");
+                    }//End nested if/else
+
+     }//End else if
+
+    else if (strcmp(service1, "Car") == 0) 
+    {  
+    
+            scanf("%s", service2);
+            if (strcmp(service2, "wash") == 0)
+                    {
+                    printf("You entered: %s %s.\n", service1, service2);
+                    printf("Cost of car wash: $%d\n", wash);
+                    }
+            else
+                    {
+                    printf("You entered: %s\n", service1);
+                    printf("Requested service is not recognized\n");
+                    }//End nested if/else
+
+     }//End else if
+
     else
         {
-            printf("You entered: %s\n", service);
+            scanf("%s", service2);
+            printf("You entered: %s %s\n", service1, service2);
             printf("Requested service is not recognized\n");
         }
 
--- mod4/hw3ch3pr2.c
+++ mod4/hw3ch3pr2.c
@@ -26,85 +26,180 @@
 int main()
 {
     //Declare variables
-    char fService[30];
-    char sService[30];
+    char fService1[30];
+    char fService2[30];
+    char sService1[20];
+    char sService2[20];
     int costOne;
     int costTwo;
     int total;
 
     //Display menu to customer
-    printf("Davy's auto shop services\n Oil change -- $35\n Tire rotation -- $19\n Car wash -- $7\n Car wax -- $12\n Enter - for no service\n");
+    printf("Davy's auto shop services\n Oil change -- $35\n");
+    printf("Tire rotation -- $19\n Car wash -- $7\n Car wax -- $12\n");
+    printf("Enter - for no service\n");
      
     //Receive customer service request
     printf("Select first service:\n");
-    scanf("%[^\n]", fService);
-    printf("Select second service:\n");
-    scanf("%[^\n]", sService);
-
-    //Service 1
-    if (strcmp(fService, "Oil change") == 0)
-        { 
-            printf("Service 1: %s, $35\n", fService);
-            costOne = 35;
-        } 
-    else if  (strcmp(fService, "Tire rotation") == 0)
-        { 
-            printf("Service 1: %s, $19\n", fService);
-            costOne = 19;
-        } 
-    else if  (strcmp(fService, "Car wash") == 0)
-        { 
-            printf("Service 1: %s, $7\n", fService);
-            costOne = 7;
-        } 
-    else if  (strcmp(fService, "Car wax") == 0)
-        { 
-            printf("Service 1: %s, $12\n", fService);
-            costOne = 12;
-        }    
-    else if  (strcmp(fService, "-") == 0)
-        { 
-            printf("Service 1: %s, $0\n", fService);
-            costOne = 0;
-        } 
-    else
-        { 
-            printf("Error: Requested service not available");
-            costOne = 0;
-        }
-
-    //Service 2
-  if (strcmp(sService, "Oil change") == 0)
-        { 
-            printf("Service 1: %s, $35\n", sService);
-            costTwo = 35;
-        } 
-    else if  (strcmp(fService, "Tire rotation") == 0)
-        { 
-            printf("Service 1: %s, $19\n", sService);
-            costTwo = 19;
-        } 
-    else if  (strcmp(fService, "Car wash") == 0)
-        { 
-            printf("Service 1: %s, $7\n", sService);
-            costTwo = 7;
-        } 
-    else if  (strcmp(fService, "Car wax") == 0)
-        { 
-            printf("Service 1: %s, $12\n", sService);
-            costTwo = 12;
-        }    
-    else if  (strcmp(fService, "-") == 0)
-        { 
-            printf("Service 1: %s, $0\n", sService);
-            costTwo = 0;
-        } 
-    else
-        { 
-            printf("Error: Requested service not available");
-            costTwo = 0;
-        }
+    scanf("%s", fService1);
     
+    //Service 1
+    if (strcmp(fService1, "-") == 0)
+    {
+       
+        printf("Service 1: %s, $0\n", fService1);
+        costOne = 0;
+    }//End if
+    else if (strcmp(fService1, "Oil") == 0)
+    { 
+            scanf("%s", fService2);
+            if (strcmp(fService2, "change") == 0)
+            {
+            printf("Service 1: %s %s, $35\n", fService1, fService2);
+            costOne = 35;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costOne = 0;
+            }//End nested else/if
+
+     }//End else if
+
+     else if (strcmp(fService1, "Tire") == 0)
+    { 
+            scanf("%s", fService2);
+            if (strcmp(fService2, "rotation") == 0)
+            {
+            printf("Service 1: %s %s, $19\n", fService1, fService2);
+            costOne = 19;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costOne = 0;
+            }//End nested else/if
+     }//End else if
+
+
+   else if (strcmp(fService1, "Car") == 0)
+     { 
+            scanf("%s", fService2);
+            if (strcmp(fService2, "wash") == 0)
+            {
+            printf("Service 1: %s %s, $7\n", fService1, fService2);
+            costOne = 7;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costOne = 0;
+            }//End nested else/if
+    }//End else if
+
+    else if (strcmp(fService1, "Car") == 0)
+    { 
+            scanf("%s", fService2);
+            if (strcmp(fService2, "wax") == 0)
+            {
+            printf("Service 1: %s %s, $12\n", fService1, fService2);
+            costOne = 12;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costOne = 0;
+            }//End nested else/if 
+
+     }//End else if
+
+    else
+    {
+           printf("Error: Requested service not available\n");
+            costOne = 0;
+
+    }//End else
+    
+    //Service 2
+    printf("Select second service:\n");
+    scanf("%s", sService1);
+
+  if (strcmp(sService1, "-") == 0)
+    {
+       
+        printf("Service 1: %s, $0\n", sService1);
+        costOne = 0;
+    }//End if
+
+        else if (strcmp(sService1, "Oil") == 0)
+        { 
+            scanf("%s", sService2);
+            if (strcmp(sService2, "change") == 0)
+            {
+            printf("Service 1: %s %s, $35\n", sService1, sService2);
+            costTwo = 35;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costTwo = 0;
+            }//End nested else/if
+
+        }//End else if
+
+        else if (strcmp(sService1, "Tire") == 0)
+        { 
+            scanf("%s", sService2);
+            if (strcmp(sService2, "rotation") == 0)
+            {
+            printf("Service 1: %s %s, $19\n", sService1, sService2);
+            costTwo = 19;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costTwo = 0;
+            }//End nested else/if
+        }//End else if
+
+
+   else if (strcmp(sService1, "Car") == 0)
+        { 
+            scanf("%s", sService2);
+            if (strcmp(sService2, "wash") == 0)
+            {
+            printf("Service 1: %s %s, $7\n", sService1, sService2);
+            costTwo = 7;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costTwo = 0;
+            }//End nested else/if
+        }//End else if
+
+    else if (strcmp(sService1, "Car") == 0)
+        { 
+            scanf("%s", sService2);
+            if (strcmp(sService2, "wax") == 0)
+            {
+            printf("Service 1: %s %s, $12\n", sService1, sService2);
+            costTwo = 12;
+            }
+            else
+            { 
+            printf("Error: Requested service not available\n");
+            costTwo = 0;
+            }//end nested if/else
+        }//end else if
+  else
+    {
+           printf("Error: Requested service not available\n");
+           costTwo = 0;
+
+    }//End else
+  
+      
     //Calculate total & display to customer
     total = costOne + costTwo;
     printf("Total: $%d\n", total);
