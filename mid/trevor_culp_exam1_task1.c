d031a432e88e7fb5001dae78bb09e910c357b77e
Trevor <trevorculp@mail.weber.edu>
2018-02-27 08:44:42 -0700

    Initial commit

---
+++ mid/trevor_culp_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trevor_culp_exam1_task1.c
+ *
+ *    Description:  First choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:02 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Trevor Culp (), Trevorculp@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mid/trevor_culp_exam1_task2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trevor_culp_exam1_task2.c
+ *
+ *    Description:  Second choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Trevor Culp (), Trevorculp@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
