63175303f3c5a53d3f991fdde1917b79c2c30f38
Trevor <trevorculp@mail.weber.edu>
2018-03-01 09:09:46 -0700

    update

--- mid/trevor_culp_exam1_task1.c
+++ mid/trevor_culp_exam1_task1.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  trevor_culp_exam1_task1.c
  *
- *    Description:  First choice
+ *    Description:  First choice: Task 1
  *
  *        Version:  1.0
  *        Created:  02/27/2018 08:42:02 AM
@@ -20,13 +20,46 @@
 // Constants
 
 // Function Prototypes
-
+void MoneyOptions(float moneyEarned);
 // Main Function
 int main()
 {
+    //Initialize variable
+    float moneyEarned = 0;
+
+    //Prompt for the desired amount of money earned
+    printf("How much money would you like to earn?\n");
+    scanf("%f", &moneyEarned);
+
+    MoneyOptions(moneyEarned);
 
     return 0;
 }
 // Function Definitions
 
+//Func: MoneyOptions
+//Displays what can be done with the desired amount of money earned 
+void MoneyOptions(float moneyEarned)
+{
+    if(moneyEarned <= 5000)
+    {
+        printf("With this amount of money %.2f you can buy a TV\n", moneyEarned);
+    }//End if
+
+    else if((moneyEarned >= 5000) && (moneyEarned <= 10000))
+    {
+        printf("With this amount of money %.2f you can buy a RTV\n", moneyEarned);
+    }//End else if
+
+    else if((moneyEarned > 10000) && (moneyEarned <= 50000))
+    {
+        printf("With this amount of money %.2f you can buy a car\n", moneyEarned);
+    }//End else if
+    
+    else if(moneyEarned > 50000)
+    {
+        printf("With this amount of money %.2f you can buy a small house\n", moneyEarned);
+    }//End else if
+    return;
+}//End MoneyOptions
 
